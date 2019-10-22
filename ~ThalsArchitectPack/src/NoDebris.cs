24c2da5881cd373b3803291269ea42453175af96
Victor <Thalassicus@hotmail.com>
2019-10-22 09:30:56 -0500

    Architect Pack

---
+++ ~ThalsArchitectPack/src/NoDebris.cs
@@ -0,0 +1,27 @@
+﻿using Verse;
+using HugsLib;
+using Harmony;
+
+namespace NoDebris
+{
+    public class NoDebris : ModBase
+    {
+        public override string ModIdentifier
+        {
+            get { return "NoDebris"; }
+        }
+    }
+
+    // original method: public override bool Verse.SectionLayer_TerrainScatter.Visible
+    [HarmonyPatch(typeof(SectionLayer_TerrainScatter), "Visible", MethodType.Getter)]
+
+    internal static class HideTerrainScatter
+    {
+        [HarmonyPrefix]
+        public static bool Patch_SectionLayer_TerrainScatter_Visible(SectionLayer_TerrainScatter __instance, ref bool __result)
+        {
+            __result = false;
+            return false;
+        }
+    }
+}
